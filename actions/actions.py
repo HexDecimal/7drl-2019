2dbc3a0d71f1433e78b6243fad4cf9868a535cf8
Kyle Stewart <4b796c65@gmail.com>
2019-03-03 14:36:31 -0800

    Added basic combat.

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Optional, TYPE_CHECKING
 
 import engine.entity
+import engine.graphic
+if TYPE_CHECKING:
+    import engine.location
 
 
 class Action:
@@ -8,9 +11,11 @@ class Action:
         self.entity = entity
 
     def invoke(self) -> bool:
-        assert self.entity.actor
+        """Attempt the action and return True if the action was performed."""
+        assert self.entity.actor, "Action invoked on a non-actor entity."
         # Ensure this actor was not already scheduled.
-        assert self.entity.actor.ticket is None
+        assert self.entity.actor.ticket is None, \
+            "Actor is already waiting after an action."
         if not self.poll():
             return False
         interval = self.action()
@@ -18,9 +23,14 @@ class Action:
         return True
 
     def poll(self) -> bool:
+        """Return True if this action would be valid."""
         return True
 
     def action(self) -> int:
+        """Perform the action.
+
+        This should never be called unless poll returns True.
+        """
         raise NotImplementedError()
 
 
@@ -29,7 +39,8 @@ class Wait(Action):
         return 100
 
 
-class Move(Action):
+class BumpAction(Action):
+    """An action with a direction."""
     def __init__(
         self,
         entity: engine.entity.Entity,
@@ -38,19 +49,40 @@ class Move(Action):
         super().__init__(entity)
         self.direction = direction
 
+    def get_destination(self) -> "engine.location.Location":
+        """Return the location at the destination."""
+        return self.entity.location.get_relative(*self.direction)
+
+
+class Move(BumpAction):
     def poll(self) -> bool:
-        new_loc = self.entity.location.get_relative(*self.direction)
-        assert new_loc
-        for obj in new_loc.contents:
+        for obj in self.get_destination().contents:
             if obj.actor:
                 return False
         return True
 
     def action(self) -> int:
-        new_loc = self.entity.location.get_relative(*self.direction)
-        self.entity.location = new_loc
+        self.entity.location = self.get_destination()
         if self.direction[0] and self.direction[1]:
             return 150
+        return 100
+
+
+class BumpAttack(BumpAction):
+    def get_target(self) -> Optional[engine.entity.Entity]:
+        for obj in self.get_destination().contents:
+            if obj.actor:
+                return obj
+        return None
+
+    def poll(self) -> bool:
+        return bool(self.get_target())
+
+    def action(self) -> int:
+        target = self.get_target()
+        assert target
+        target.actor = None
+        target.graphic = engine.graphic.Graphic(ord('%'), (63, 63, 63))
         return 100
 
 
@@ -63,8 +95,17 @@ class Bump(Action):
         super().__init__(entity)
         self.direction = direction
 
+    ACTIONS = (Move, BumpAttack)
+
     def poll(self) -> bool:
-        return Move(self.entity, self.direction).poll()
+        for action_type in self.ACTIONS:
+            if action_type(self.entity, self.direction).poll():
+                return True
+        return False
 
     def action(self) -> int:
-        return Move(self.entity, self.direction).action()
+        for action_type in self.ACTIONS:
+            action = action_type(self.entity, self.direction)
+            if action.poll():
+                return action.action()
+        assert False  # Will not be reached if poll returns True.
--- engine/graphic.py
+++ engine/graphic.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import engine.component
 
@@ -7,5 +7,13 @@ class Graphic(engine.component.Component):
     CH = ord('!')
     FG = (255, 255, 255)
 
+    def __init__(
+        self,
+        ch: Optional[int] = None,
+        fg: Optional[Tuple[int, int, int]] = None,
+    ):
+        self.ch = ch if ch is not None else self.CH
+        self.fg = fg if fg is not None else self.FG
+
     def get(self) -> Tuple[int, Tuple[int, int, int]]:
-        return self.CH, self.FG
+        return self.ch, self.fg
