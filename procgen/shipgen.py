22c3e01cb96f1527d4fd29f79b1ac501f52ede3d
Kyle Stewart <4b796c65@gmail.com>
2019-03-05 21:38:07 -0800

    Reduce number of doorways.

--- procgen/shipgen.py
+++ procgen/shipgen.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import sys
+
 import random
 from typing import Iterator, NamedTuple, Optional, Tuple
 
@@ -27,32 +29,69 @@ class RoomType(NamedTuple):
 
 
 class ShipRoomConntector(AbstractGrowingTree[Tuple[int, int, int]]):
+    CARDINALS = ((-1, 0), (1, 0), (0, -1), (0, 1))
+
     def __init__(self, ship: Ship):
         self.ship = ship
         self.visited = ship.rooms == -1
         self.visited[self.ship.root_node] = True
-        super().__init__([self.ship.root_node])
+        super().__init__()
+        self.visit(self.ship.root_node, None)
+
+    def grow(self) -> None:
+        """Run a single step of the growing tree algorithm."""
+        assert self.stem
+        neighbors = list(self.get_all_neighbors())
+        if not neighbors:
+            self.stem = []
+            return
+        nodes, weights = zip(*neighbors)
+        neighbor, stem = self.ship.rng.choices(list(nodes), weights)[0]
+        self.visit(neighbor, stem)
+        if not list(self.get_neighbors(stem)):
+            self.stem.remove(stem)
+
+    def get_all_neighbors(
+        self,
+    ) -> Iterator[Tuple[Tuple[Tuple[int, int, int],
+                              Tuple[int, int, int]], int]]:
+        for node in self.stem:
+            for neighbor, weight in self.get_neighbors(node):
+                yield (neighbor, node), weight
 
     def select_stem(self) -> int:
         return self.ship.rng.randint(0, len(self.stem) - 1)
 
     def select_neighbor(
-        self,
-        node: Tuple[int, int, int],
+        self, node: Tuple[int, int, int],
     ) -> Optional[Tuple[int, int, int]]:
         """Return an unvisited neighbor to `node` if one exists.
 
         This function should be overridden to set the behavior
         """
-        CARDINALS = ((-1, 0), (1, 0), (0, -1), (0, 1))
-        for x, y in self.ship.rng.sample(CARDINALS, 4):
+        neighbors = list(self.get_neighbors(node))
+        if not neighbors:
+            return None
+        nodes, weights = zip(*self.get_neighbors(node))
+        return self.ship.rng.choices(list(nodes), weights)[0]  # type: ignore
+
+    def get_neighbors(
+        self, node: Tuple[int, int, int],
+    ) -> Iterator[Tuple[Tuple[int, int, int], int]]:
+        """Generate an iterator of (neighbor, weight) tuples."""
+        for x, y in self.CARDINALS:
             neighbor = node[0] + x, node[1] + y, node[2]
             if not(0 <= neighbor[0] < self.visited.shape[0]
                    and 0 <= neighbor[1] < self.visited.shape[1]):
                 continue
-            if not self.visited[neighbor]:
-                return neighbor
-        return None
+            if self.visited[neighbor]:
+                continue
+            if self.ship.rooms[node] == self.ship.rooms[neighbor]:
+                yield neighbor, 8
+            yield neighbor, 1
+
+    def get_room_type(self, node: Tuple[int, int, int]) -> RoomType:
+        return self.ship.room_types[self.ship.rooms[node]]
 
     def visit(
         self,
@@ -61,13 +100,19 @@ class ShipRoomConntector(AbstractGrowingTree[Tuple[int, int, int]]):
     ) -> None:
         super().visit(node, prev)
         self.visited[node] = True
+        if not list(self.get_neighbors(node)):
+            self.stem.remove(node)
         if prev is None:
             return
         if prev[2] != node[2]:
             return
         if prev > node:  # Make sure prev is on the upper-left side of node.
             prev, node = node, prev
-        self.connect_rooms_debug(prev, node)
+
+        if "--debug" in sys.argv:
+            self.connect_rooms_debug(prev, node)
+        else:
+            self.connect_rooms(prev, node)
 
     def connect_rooms(
         self, room_a: Tuple[int, int, int], room_b: Tuple[int, int, int],
@@ -80,8 +125,10 @@ class ShipRoomConntector(AbstractGrowingTree[Tuple[int, int, int]]):
             assert room_a[1] == room_b[1]
             door_y += self.ship.rng.randint(1, self.ship.room_height - 1)
         door = door_x, door_y, room_b[2]
-        self.ship.zone.data["tile"][door] = \
-            tiles.metal_floor._replace(bg=(0, 255, 0))
+        self.ship.zone.data["tile"][door] = max(
+            self.ship.room_types[self.ship.rooms[room_a]],
+            self.ship.room_types[self.ship.rooms[room_b]],
+        ).floor
 
     def connect_rooms_debug(
         self, room_a: Tuple[int, int, int], room_b: Tuple[int, int, int],
@@ -180,8 +227,7 @@ class Ship:
                        floor: int) -> Tuple[slice, slice]:
         """Return the slice indexes of a an unclaimed area."""
         width, height = size
-        room_area = np.zeros((width * 2 - 1, height * 2 - 1), dtype=bool)
-        room_area[:width, :height] = True
+        room_area = np.ones((width, height), dtype=bool)
         #  Convolve room_area into self.rooms, values of zero mean this room
         #  will fit.
         valid = scipy.signal.convolve(
