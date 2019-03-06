c4b2ca194c8b5f970b13f3df0368f6fd030b3c4e
Kyle Stewart <4b796c65@gmail.com>
2019-03-05 20:11:01 -0800

    Rooms are now connected.

--- procgen/shipgen.py
+++ procgen/shipgen.py
@@ -5,7 +5,9 @@ from typing import Iterator, NamedTuple, Optional, Tuple
 
 import numpy as np  # type: ignore
 import scipy.signal  # type: ignore
+import tcod
 
+from procgen.growingtree import AbstractGrowingTree
 import engine.zone
 import tiles
 
@@ -22,6 +24,76 @@ class RoomType(NamedTuple):
     priority: float = 0
     floor: tiles.Tile = tiles.metal_floor
     wall: tiles.Tile = tiles.metal_wall
+
+
+class ShipRoomConntector(AbstractGrowingTree[Tuple[int, int, int]]):
+    def __init__(self, ship: Ship):
+        self.ship = ship
+        self.visited = ship.rooms == -1
+        self.visited[self.ship.root_node] = True
+        super().__init__([self.ship.root_node])
+
+    def select_stem(self) -> int:
+        return self.ship.rng.randint(0, len(self.stem) - 1)
+
+    def select_neighbor(
+        self,
+        node: Tuple[int, int, int],
+    ) -> Optional[Tuple[int, int, int]]:
+        """Return an unvisited neighbor to `node` if one exists.
+
+        This function should be overridden to set the behavior
+        """
+        CARDINALS = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        for x, y in self.ship.rng.sample(CARDINALS, 4):
+            neighbor = node[0] + x, node[1] + y, node[2]
+            if not(0 <= neighbor[0] < self.visited.shape[0]
+                   and 0 <= neighbor[1] < self.visited.shape[1]):
+                continue
+            if not self.visited[neighbor]:
+                return neighbor
+        return None
+
+    def visit(
+        self,
+        node: Tuple[int, int, int],
+        prev: Optional[Tuple[int, int, int]],
+    ) -> None:
+        super().visit(node, prev)
+        self.visited[node] = True
+        if prev is None:
+            return
+        if prev[2] != node[2]:
+            return
+        if prev > node:  # Make sure prev is on the upper-left side of node.
+            prev, node = node, prev
+        self.connect_rooms_debug(prev, node)
+
+    def connect_rooms(
+        self, room_a: Tuple[int, int, int], room_b: Tuple[int, int, int],
+    ) -> None:
+        door_x = room_b[0] * self.ship.room_width
+        door_y = room_b[1] * self.ship.room_height
+        if room_a[0] == room_b[0]:
+            door_x += self.ship.rng.randint(1, self.ship.room_width - 1)
+        else:
+            assert room_a[1] == room_b[1]
+            door_y += self.ship.rng.randint(1, self.ship.room_height - 1)
+        door = door_x, door_y, room_b[2]
+        self.ship.zone.data["tile"][door] = \
+            tiles.metal_floor._replace(bg=(0, 255, 0))
+
+    def connect_rooms_debug(
+        self, room_a: Tuple[int, int, int], room_b: Tuple[int, int, int],
+    ) -> None:
+        line = tcod.line_where(
+            room_a[0] * self.ship.room_width + 2,
+            room_a[1] * self.ship.room_height + 2,
+            room_b[0] * self.ship.room_width + 2,
+            room_b[1] * self.ship.room_height + 2,
+        )
+        self.ship.zone.data["tile"][line] = \
+            tiles.metal_floor._replace(bg=(0, 255, 0))
 
 
 class Ship:
@@ -63,6 +135,7 @@ class Ship:
                     self.add_room(self.new_room(), (size, size))
             except NoRoom:
                 pass
+        self.rooms[self.rooms == 0] = 1
 
         self.finalize()
 
@@ -84,6 +157,7 @@ class Ship:
         start_x = self.rng.randint(0, self.length // 4)
         end_x = self.rooms.shape[0] - self.rng.randint(0, self.length // 4)
         self.rooms[start_x:end_x, self.half_width, 0] = 1
+        self.root_node = start_x, self.half_width, 0
         self.start_position = (start_x * self.room_width + 1,
                                self.half_width * self.room_height + 1, 0)
 
@@ -104,6 +178,7 @@ class Ship:
 
     def get_free_space(self, size: Tuple[int, int],
                        floor: int) -> Tuple[slice, slice]:
+        """Return the slice indexes of a an unclaimed area."""
         width, height = size
         room_area = np.zeros((width * 2 - 1, height * 2 - 1), dtype=bool)
         room_area[:width, :height] = True
@@ -112,13 +187,13 @@ class Ship:
         valid = scipy.signal.convolve(
             (self.rooms[..., floor] != 0).astype(int),
             room_area,
-            "same",
-        )[:-width, :-height]
-        valid = (valid == 0).nonzero()
-        if not valid[0].size:
+            "full",
+        )[width-1:-width+1, height-1:-height+1]
+        valid = np.transpose((valid == 0).nonzero())
+        if not valid.size:
             raise NoRoom("No space left for room.")
-        i = self.rng.randint(0, len(valid[0]) - 1)
-        x, y = valid[0][i], valid[1][i]
+        x, y = self.rng.choice(valid)
+        assert (self.rooms[x:x+width, y:y+height, floor] == 0).all()
         return slice(x, x + width), slice(y, y + height)
 
     def gen_rooms(self) -> None:
@@ -196,6 +271,8 @@ class Ship:
             self.zone.data["tile"][left, top+1:bottom, cz] = left_tile
             self.zone.data["tile"][left+1:right, top+1:bottom, cz] = \
                 room_type.floor
+
+        ShipRoomConntector(self).generate()
 
     def show(self) -> str:
         def icon(x: int, y: int) -> str:
